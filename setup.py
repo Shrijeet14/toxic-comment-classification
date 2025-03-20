c25db4611c54df248febaf76fad70a7273195ec7
shrijeet kumar <127421688+Shrijeet14@users.noreply.github.com>
2025-03-20 17:57:22 +0530

    Initial Commit made the project directory

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This Project aims at using different ML / Dl models to experiment and classify toxic comments.',
+    author='Shrijeet Kumar',
+    license='',
+)
